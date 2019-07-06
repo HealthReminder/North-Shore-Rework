46df64f51c7d3ba0635153fc7ac9e0d8be356f9c
Eduardo-PC <devugates@dronekids.com.br>
2019-07-06 20:43:49 -0300

    Changed intro + Colors + Resolution

--- NorthShore/Assets/Scripts/Reworked/Behaviours/SupportDevelopersBehaviour.cs
+++ NorthShore/Assets/Scripts/Reworked/Behaviours/SupportDevelopersBehaviour.cs
@@ -5,14 +5,18 @@ using UnityEngine;
 public class SupportDevelopersBehaviour : MonoBehaviour
 {
     public bool isOn = false;
+    public float minExistence = 2.5f;
+    float currentExistence = 0;
     public Transform container;
     private void Start() {
         ToggleSupportGUI(true);
     }
     private void Update() {
-        if(isOn)
-            if(Input.anyKeyDown || Input.GetMouseButton(0))
-                ToggleSupportGUI(false);
+        if(currentExistence <= minExistence)
+            currentExistence+=Time.deltaTime;
+        else if(isOn)
+                if(Input.anyKeyDown || Input.GetMouseButton(0))
+                    ToggleSupportGUI(false);
     }
 
     private void ToggleSupportGUI(bool switchTo) {
@@ -29,13 +33,13 @@ public class SupportDevelopersBehaviour : MonoBehaviour
                 yield return null;
             }
         }else
-        while(!isOn){
-            if(container.localPosition.y > -1000)
-                container.localPosition += new Vector3(0,-10,0);
-            else 
-                container.gameObject.SetActive(false);
-            yield return null;
-        }
+            while(!isOn){
+                if(container.localPosition.y > -1000)
+                    container.localPosition += new Vector3(0,-10,0);
+                else 
+                    container.gameObject.SetActive(false);
+                yield return null;
+            }
         yield break;
     }
 }
